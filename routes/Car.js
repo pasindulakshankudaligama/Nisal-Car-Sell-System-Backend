862df291fecb17a4555d6f584990a3427614f95b
pasindulakshankudaligama <pasindulakshankudaligama@gmail.com>
2022-10-08 18:02:14 +0530

    car post method added

--- routes/Car.js
+++ routes/Car.js
@@ -13,4 +13,18 @@ router.get('/',async (req,res)=>{
     }
 })
 
+router.post('/',async (req,res)=>{
+    const cars=new Car({
+        regNo:req.body.regNo,
+        description:req.body.description,
+        brandName:req.body.brandName
+    })
+    try{
+        const response=await cars.save();
+        res.json(response)
+    }catch (e) {
+        res.send('error :'+e)
+    }
+})
+
 module.exports = router;
